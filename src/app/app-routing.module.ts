582fff1ef14318e2f5736e54a46e3c80e2befa94
Shivraj Pattanshetti <kotiamaru@gmail.com>
2022-01-30 21:19:00 -0500

    second commit dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/home/about/about.component';
+import { ContactComponent } from './components/home/contact/contact.component';
+import { DashboardComponent } from './components/home/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { PublicationComponent } from './components/home/publication/publication.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'dashboard' ,pathMatch:'full'},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'about', component:AboutComponent},
+  {path:'publication', component:PublicationComponent},
+  {path:'**', redirectTo:'dashboard'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/home/dashboard/dashboard.component';
+import { ContactComponent } from './components/home/contact/contact.component';
+import { AboutComponent } from './components/home/about/about.component';
+import { PublicationComponent } from './components/home/publication/publication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    ContactComponent,
+    AboutComponent,
+    PublicationComponent
   ],
   imports: [
     BrowserModule,
